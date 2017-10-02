75b81c1fc6fa518e6e343fb0ff4b271b85006557
Nicolas DELAUNEY <niko-d@live.fr>
2017-10-02 22:59:39 +0100

    :art: Add car

--- Docs/gallery.php
+++ Docs/gallery.php
@@ -54,8 +54,20 @@
           <div class="container column is-6">
             <article class="message  has-background-alpha">
               <div class="message-body">
-                <p class="title is-4">RedHouse</p>
+                <p class="title is-4">Red House</p>
                 <p class="has-text-centered"><a href="../JS/assets/RedHouse.png"><img src="../JS/assets/RedHouse.png" alt="redhouse"></a></p>
+              </div>
+            </article>
+            <article class="message  has-background-alpha">
+              <div class="message-body">
+                <p class="title is-4">Colonial House</p>
+                <p class="has-text-centered"><a href="../JS/assets/ColonialHouse.png"><img src="../JS/assets/ColonialHouse.png" alt="colonialhouse"></a></p>
+              </div>
+            </article>
+            <article class="message  has-background-alpha">
+              <div class="message-body">
+                <p class="title is-4">Italiante House</p>
+                <p class="has-text-centered"><a href="../JS/assets/ItalianteHouse.png"><img src="../JS/assets/ItalianteHouse.png" alt="italiantehouse"></a></p>
               </div>
             </article>
           </div>
@@ -66,22 +78,10 @@
                 <p class="has-text-centered"><a href="../JS/assets/Building1.png"><img src="../JS/assets/Building1.png" alt="building1"></a></p>
               </div>
             </article>
-          </div>
-        </div>
-        <div class="columns">
-          <div class="container column is-6">
-            <article class="message  has-background-alpha">
+            <article class="message has-background-alpha">
               <div class="message-body">
-                <p class="title is-4">ColonialHouse</p>
-                <p class="has-text-centered"><a href="../JS/assets/ColonialHouse.png"><img src="../JS/assets/ColonialHouse.png" alt="colonialhouse"></a></p>
-              </div>
-            </article>
-          </div>
-          <div class="container column is-6">
-            <article class="message  has-background-alpha">
-              <div class="message-body">
-                <p class="title is-4">ItalianteHouse</p>
-                <p class="has-text-centered"><a href="../JS/assets/ItalianteHouse.png"><img src="../JS/assets/ItalianteHouse.png" alt="italiantehouse"></a></p>
+                <p class="title is-4">Red Car</p>
+                <p class="has-text-centered"><a href="../JS/assets/car1.gif"><img src="../JS/assets/car1.1.gif" alt="car1"></a></p>
               </div>
             </article>
           </div>
