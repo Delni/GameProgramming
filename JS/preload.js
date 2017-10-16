da3bccbf5877546f71d5c07ab6ea85bf3fc2a8b7
Nicolas DELAUNEY <niko-d@live.fr>
2017-10-16 17:13:45 +0100

    :tractor: new File structure

--- JS/preload.js
+++ JS/preload.js
@@ -2,12 +2,12 @@ var preload = function(){};
 
 preload.prototype = {
   preload: function() {
-    game.load.image('sky', '../JS/assets/sky.png');
-    game.load.image('building1','../JS/assets/RedHouse.png');
-    game.load.image('ground', '../JS/assets/platform.png');
-    game.load.image('star', '../JS/assets/star.png');
-    game.load.spritesheet('dude', '../JS/assets/player_spritesheet.png', 138, 150);
-    game.load.spritesheet('buttonPlay', '../JS/assets/buttonPlay.png', 195, 72);
+    game.load.image('sky', '../JS/assets/Decor/sky.png');
+    game.load.image('building1','../JS/assets/Decor/Buildings/MansardeHouse.png');
+    game.load.image('ground', '../JS/assets/Decor/platform.png');
+    game.load.image('star', '../JS/assets/Elements/star.png');
+    game.load.spritesheet('dude', '../JS/assets/Player/player_spritesheet.png', 138, 150);
+    game.load.spritesheet('buttonPlay', '../JS/assets/Inputs/buttonPlay.png', 195, 72);
   },
 
   create: function(){
