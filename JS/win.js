0e212e60740ab16b89dbaba7cd025c6fd9bfb1fe
Nicolas DELAUNEY <niko-d@live.fr>
2017-11-06 18:28:06 +0000

    :sparkles: Win menu ! Allows to navigate through levels

--- JS/win.js
+++ JS/win.js
@@ -6,6 +6,25 @@ win.prototype = {
   },
 
   create: function(){
-    game.state.start('Menu')
+    game.add.sprite(0, 0, 'WinWP');
+    group = game.add.group();
+
+    var replayButton = game.make.button(game.world.width - 200, game.world.height - 77, 'large_buttons', replay, this, 11, 9, 10);
+    var nextButton = game.make.button(5 , game.world.height - 77, 'large_buttons', nxtLvl, this, 14, 12, 13);
+    var menuButton = game.make.button(5 , 5 , 'large_buttons', backGame, this, 17, 15, 16);
+    var lvlButton = game.make.button(game.world.width - 200 , 5, 'large_buttons', playGame, this, 20, 18, 19);
+
+    group.add(replayButton);
+    group.add(nextButton);
+    group.add(menuButton);
+    group.add(lvlButton);
   },
+}
+
+function nxtLvl() {
+  game.state.start('Game',true,false,currentLvl);
+}
+
+function replay() {
+  game.state.start('Game',true,false,currentLvl+1);
 }
