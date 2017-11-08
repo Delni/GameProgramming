f7b4d866c7181cfa2955c1543929e7236fe40c80
Nicolas DELAUNEY <niko-d@live.fr>
2017-11-08 17:50:34 +0000

    :sparkles: Add Lose menu

--- JS/lose.js
+++ JS/lose.js
@@ -2,10 +2,18 @@ var lose = function(){};
 
 lose.prototype = {
   preload: function() {
-
+    game.load.image('LoseWP','../JS/assets/LoseWP.png');
   },
 
   create: function(){
+    music.fadeOut(1000);
+    game.add.sprite(0, 0, 'LoseWP');
+    group = game.add.group();
 
+    var replayButton = game.make.button(game.world.centerX - 200, game.world.height - 150, 'large_buttons', replay, this, 14, 12, 13);
+    var menuButton = game.make.button(game.world.centerX + 5, game.world.height - 150, 'large_buttons', backGame, this, 17, 15, 16);
+
+    group.add(replayButton);
+    group.add(menuButton);
   },
 }
