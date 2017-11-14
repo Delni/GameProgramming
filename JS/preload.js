8c5572913174e70b9b390ccf5c9d0eaeed1e79cb
Unknown <claire.lacroix@minesdedouai.fr>
2017-11-14 14:44:06 +0100

    :art: add obstacles assets on preload

--- JS/preload.js
+++ JS/preload.js
@@ -10,16 +10,20 @@ preload.prototype = {
     game.load.image('hugeclouds', '../JS/assets/Decor/parallax_backgound_pack/_07_huge_clouds.png');
     game.load.image('clouds', '../JS/assets/Decor/parallax_backgound_pack/_08_clouds.png');
     // City
+      //Buildings
     game.load.image('Building1','../JS/assets/Decor/Buildings/Building1.png');
     game.load.image('ColonialHouse','../JS/assets/Decor/Buildings/ColonialHouse.png');
     game.load.image('ItalianteHouse','../JS/assets/Decor/Buildings/ItalianteHouse.png');
     game.load.image('MansardeHouse','../JS/assets/Decor/Buildings/MansardeHouse.png');
     game.load.image('RedHouse','../JS/assets/Decor/Buildings/RedHouse.png');
-    game.load.image('ground', '../JS/assets/Decor/pedWayDown.png');
-    // game.load.image('stars', '../JS/assets/Elements/star.png');
-    game.load.image('road', '../JS/assets/Decor/upPedWay.png');
     game.load.image('lastBuilding_back', '../JS/assets/Decor/Buildings/Last_up.png');
     game.load.image('lastBuilding_front', '../JS/assets/Decor/Buildings/Last_down.png');
+    game.load.image('CrossingRoad', '../JS/assets/Decor/Buildings/Road.png');
+      //Road and pedestrian ways
+    game.load.image('ground', '../JS/assets/Decor/pedWayDown.png');
+    game.load.image('road', '../JS/assets/Decor/upPedWay.png');
+    game.load.image('roadDownCorner', '../JS/assets/Decor/RoadDownCorner.png');
+    game.load.image('roadUpCorner', '../JS/assets/Decor/RoadUpCorner.png');
     // Player
     game.load.spritesheet('dude', '../JS/assets/Player/player_spritesheet.png', 138, 150);
     game.load.spritesheet('newspaper', '../JS/assets/Elements/newspaper_spritesheet.png', 96, 96);
@@ -27,6 +31,9 @@ preload.prototype = {
     game.load.spritesheet('box', '../JS/assets/Decor/Obstacles/Box.png');
     game.load.spritesheet('bin', '../JS/assets/Decor/Obstacles/Bin.png');
     game.load.spritesheet('lampPost', '../JS/assets/Decor/Obstacles/LampPost.png');
+    game.load.spritesheet('metroUp', '../JS/assets/Decor/Obstacles/MetroUp.png');
+    game.load.spritesheet('metroLeftSide', '../JS/assets/Decor/Obstacles/MetroLeftSide.png');
+    game.load.spritesheet('metroRightSide', '../JS/assets/Decor/Obstacles/MetroRightSide.png');
     //Font
     game.load.bitmapFont('ParkLane','../JS/assets/font/parklane.png','../JS/assets/font/parklane.fnt')
     // UI
